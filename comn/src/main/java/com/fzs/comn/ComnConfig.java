483ab076f9b0132c00b2b6f8d756497e7847d8cc
tbcc911@163.com <EveMall911>
2020-03-05 16:38:00 +0800

    hzh

--- comn/src/main/java/com/fzs/comn/ComnConfig.java
+++ comn/src/main/java/com/fzs/comn/ComnConfig.java
@@ -21,6 +21,7 @@ public class ComnConfig extends BaseInitData {
 //        http_client_url = "https://interface.app.xdxjzw.com/api/"; //服务器(香港)
         //        http_client_url = "http://47.244.219.14:8092/api/"; //服务器
                 http_client_url = "http://47.104.224.184:8092/api/"; //开发服务器
+//        http_client_url="http://192.168.199.222:8092/api/";
         http_client_key = "a$u2Dv3%NU0d4o!goS2maqXo$6bcq^qI";
         http_client_version = 100;
         ImageFrameBgImage = new int[]{R.drawable.base_image_default, R.drawable.base_image_default, R.drawable.base_image_default};
@@ -28,7 +29,7 @@ public class ComnConfig extends BaseInitData {
         SharedPreferencesFileName = "fzs_share_data";
         WX_KEY = "wx7d314006a5998a80";
 
-        base_theme_Tone = false;
+        base_theme_Tone = true;
         //是否强制登录
         force_login= false;
     }
